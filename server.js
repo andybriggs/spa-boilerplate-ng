a15e2b89dd850858aa0f1798b47767a53661ad42
Andy <andybriggs84@gmail.com>
2015-06-17 22:56:13 +0100

    added some more routing, angular still not working

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ var app = express();
 
 var env =  process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-app.use(express.static(__dirname + '/public'));
-
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
+});
 
 app.get('*', function(req, res){
 	res.render('index');
